e3071c14baae73a2f49abd2e7bd6dbf6f90e3b14
Yemyam <ethan.braum@gmail.com>
2024-09-18 23:07:14 -0600

    Completed ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,37 @@
 import { useState } from 'react'
 
+const Average = (props) => {
+  let good = props.good
+  let neutral = props.neutral
+  let bad = props.bad
+  let average
+  if ((good + neutral + bad) == 0){
+    average = 0
+  }
+  else {
+    average = (good - bad) / (good + neutral + bad)
+  }
+  return(
+    <div>average {average}</div>
+  )
+}
+
+const Positive = (props) => {
+  let good = props.good
+  let neutral = props.neutral
+  let bad = props.bad
+  let percent
+  if ((good + neutral + bad) == 0){
+    percent = 0
+  }
+  else {
+    percent = (good / (good + neutral + bad)) * 100
+  }
+  return(
+    <div>postitive {percent} %</div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,6 +50,8 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <Average good={good} bad={bad} neutral={neutral}></Average>
+      <Positive good={good} bad={bad} neutral={neutral}></Positive>
     </div>
   )
 }
