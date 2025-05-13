33ef0a34dbb5790b2980eaff9637c1c7803c597c
Yemyam <ethan.braum@gmail.com>
2025-05-13 11:57:34 -0600

    Completed ex 3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const Person = require('./models/person')
 
+/*global process*/
+
 const app = express()
 const morgan = require('morgan')
 
-morgan.token('data', function (req, res) {
-    if (req.method === 'POST') {
-      return JSON.stringify(req.body);
-    }
-    return ' ';
+morgan.token('data', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ' '
 })
 
 app.use(express.static('dist'))
@@ -18,39 +20,39 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
-    .catch(error => next(error))
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+    .catch(error => next(error))
+})
 
-app.get("/info", (request, response, next) => {
-    const date = new Date()
+app.get('/info', (request, response, next) => {
+  const date = new Date()
 
-    Person.countDocuments({}).then(count => {
-      response.send(
-        `
+  Person.countDocuments({}).then(count => {
+    response.send(
+      `
         <div>Phonebook has info for ${count} people</div>
         <div>${date}</div>
         `
     )
-    })
-      .catch(error => next(error))
-})
-
-app.get("/api/persons/:id", (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+  })
     .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -73,16 +75,16 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = new Person ({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person ({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
