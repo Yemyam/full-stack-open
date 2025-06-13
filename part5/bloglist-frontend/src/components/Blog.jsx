0b427125aeba291174ccf261a3a605f22beeb667
Yemyam <ethan.braum@gmail.com>
2025-06-13 17:20:58 -0600

    Completed ex 5.8 and 5.9

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import blogs from "../services/blogs"
 
 const Blog = ({ blog }) => {
   const [view, setView] = useState(false)
+  const [likes, setLikes] = useState(0)
+  const [liked, setLiked] = useState(false)
 
   const showWhenView = {display: view ? '' : 'none'}
   const hideWhenView = {display: view ? 'none' : ''}
+  const showWhenLiked = {display: liked ? '' : 'none'}
+  const hideWhenLiked = {display: liked ? 'none' : ''}
 
   const toggleView = () => {
     setView(!view)
@@ -18,6 +23,21 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
 
+  const handleLike =  async(event) => {
+    event.preventDefault()
+    const newBlog = {
+      ...blog,
+      likes: blog.likes + 1
+    }
+    blogs.update(newBlog)
+    setLikes(blog.likes+1)
+    setLiked(true)
+  }
+
+  useEffect(() => {
+    setLikes(blog.likes)
+  }, [])
+
   return(
   <div style={blogStyle}>
     {blog.title} {blog.author}&nbsp;
@@ -26,7 +46,8 @@ const Blog = ({ blog }) => {
     <div style={showWhenView}>
       {blog.url}
       <br></br>
-      likes {blog.likes} <button>like</button>
+      likes {likes} <button onClick={handleLike} style={hideWhenLiked}>like</button>
+      <button disabled style={showWhenLiked}>liked</button>
       <br></br>
       {blog.user.name}
     </div>
